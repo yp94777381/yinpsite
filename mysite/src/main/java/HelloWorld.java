aa414de3240e1feb3f4a692fe827299fde302aa1
yinpeng03 <yinpeng03@meituan.com>
2015-05-29 16:31:45 +0800

    hello world

---
+++ mysite/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by yinp on 15/5/29.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.println("=====");
+        System.out.println("hello world....");
+        System.out.println("-------");
+    }
+
+}
